bdaa3ad19720b5cd649bb05006aef25dba04ca3a
kewl.apps.dev1 <kewlapps.dev1@gmail.com>
2019-09-15 10:18:35 +0500

    first commit

---
+++ src/SnakeMain.java
@@ -0,0 +1,25 @@
+import javax.swing.JFrame;
+
+/**
+ * @author Ali Asjad
+ * 
+ * It was my University A.I Class Project
+ * 
+ */
+
+public class SnakeMain{
+
+
+	public static void main(String[] args) {
+		
+		System.out.println("Press A for Astar");
+		System.out.println("Press B for BFS");
+		System.out.println("Press D for DFS");
+
+
+		SnakeFrame window=new SnakeFrame();
+		window.setVisible(true);
+
+	}
+
+}
